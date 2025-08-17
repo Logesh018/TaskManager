65bc6694555f9b52086c2879eed0713bd918db28
Logesh018 <vlogesh018@gmail.com>
2025-08-17 05:31:07 +0530

    "Debugged Commit"

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
-const authRoutes = require("./routes/authRoutes")
-const userRoutes = require("./routes/userRoute")
-const taskRoutes = require("./routes/taskRoutes")
-const reportRoutes = require("./routes/reportRoutes")
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoute");
+const taskRoutes = require("./routes/taskRoutes");
+const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
 
@@ -14,36 +14,36 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-//Middleware to handle CORS
+// Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL] : ["http://your-frontend-url.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-//Middleware
+// Middleware
 app.use(express.json());
 
-//Connect DB
+// Connect DB
 connectDB();
 
-//Routes
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
 
-//Server uploads folder
+// Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-if (process.env.NODE_ENV ==="production") {
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*",(req,res) => {
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-  })
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 // Start Server
